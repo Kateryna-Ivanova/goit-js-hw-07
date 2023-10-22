a800f7af05614279658e03f97fe64499ae3f7b42
Kateryna-Ivanova <129738301+Kateryna-Ivanova@users.noreply.github.com>
2023-10-22 23:43:20 +0300

    Create gallery markup

---
+++ js/01-gallery.js
@@ -0,0 +1,42 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+// const galleryList = document.querySelector('.gallery');
+// console.log(galleryList)
+
+// function renderGalleryMarkup(images){
+//    const markup = images.map(({preview, original, description }) => {
+//     return`<li class="gallery__item">
+// <a class="gallery__link" href="${original}">
+// <img class="gallery__image" src="${preview} alt="${description}""
+// </a>
+// </li>`
+//    }).join('');
+//    galleryList.innerHTML = markup;
+// }
+// renderGalleryMarkup(galleryItems)
+// // console.log(galleryItems);
+
+
+
+
+
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList)
+
+function renderGalleryMarkup(images){
+   const markup = images
+   .map(({preview, original, description }) => {
+    return`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</li>`
+   }).join('');
+   galleryList.innerHTML = markup;
+}
+renderGalleryMarkup(galleryItems)
+// console.log(galleryItems);
