2554b3e3bfb4fce16cc41adf8269b40b1bacad1b
Kateryna-Ivanova <129738301+Kateryna-Ivanova@users.noreply.github.com>
2023-10-27 00:29:00 +0300

    add Lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,63 +1,40 @@
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
-// const galleryList = document.querySelector('.gallery');
-// console.log(galleryList)
-
-// function renderGalleryMarkup(images){
-//    const markup = images.map(({preview, original, description }) => {
-//     return`<li class="gallery__item">
-// <a class="gallery__link" href="${original}">
-// <img class="gallery__image" src="${preview} alt="${description}""
-// </a>
-// </li>`
-//    }).join('');
-//    galleryList.innerHTML = markup;
-// }
-// renderGalleryMarkup(galleryItems)
-// // console.log(galleryItems);
-
-
-
-
-
-
 
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 const galleryList = document.querySelector('.gallery');
 console.log(galleryList)
 
+// renderGalleryMarkup - ф-я розмітки галереї
 function renderGalleryMarkup(images){
    const markup = images
    .map(({preview, original, description }) => {
     return`<li class="gallery__item">
 <a class="gallery__link" href="${original}">
-<img class="gallery__image" src="${preview}" alt="${description}"/>
+<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
 </a>
 </li>`
    }).join('');
    galleryList.innerHTML = markup;
 }
+
+// Додаємо розмітку галереї на сорінці
 renderGalleryMarkup(galleryItems)
-
-
-
-
-
-
-
-
+// Додаємо реакцію на галерею
 galleryList.addEventListener('click', onClick)
 
+// ф-я того що відбуватиметься при кліку
 function onClick(evt){
-if(evt.target.nodeName === 'IMG'){
-   return;
+// Кажемо щоб прі кліку не було завантаження
+   evt.preventDefault();
+   // Кажемо, якщо це не картинка,то не реагувати на клік
+if(evt.target.nodeName !== 'IMG'){
+   return;}
+
+   const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+      instance.show();
+
+galleryList.addEventListener("keydown", (evt)=>{
+         if (evt.code === "Escape"){
+            instance.close();
+         }});
 }
-console.log("Це картинка")
-}
 
-
-
-
-
-// console.log(galleryItems);
